03c0d5afbea9a564b246f479f0940ee6f39916e4
Vladislav Trotsenko <admin@bestweb.com.ua>
2022-01-03 04:24:13 +0200

    Technical/Configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
-require 'smtp_mock'
+rspec_custom = ::File.join(::File.dirname(__FILE__), 'support/**/*.rb')
+::Dir[::File.expand_path(rspec_custom)].sort.each { |file| require file unless file[/\A.+_spec\.rb\z/] }
+
+require_relative '../lib/smtp_mock'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with(:rspec) do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
   end
+
+  config.mock_with(:rspec) do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.disable_monkey_patching!
+  config.order = :random
+
+  ::Kernel.srand(config.seed)
 end
