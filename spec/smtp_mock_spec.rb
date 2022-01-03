08e357d6a81f50d2d1caa7a44e50673205d1df1e
Vladislav Trotsenko <admin@bestweb.com.ua>
2022-01-03 04:14:01 +0200

    Technical/Add linters configs
    * Add rubocop, reek, overcommit, codeclimate configs
    * Fix rubocop issues

--- spec/smtp_mock_spec.rb
+++ spec/smtp_mock_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe SmtpMock do
-  it "has a version number" do
+  it 'has a version number' do
     expect(SmtpMock::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
